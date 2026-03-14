192a7b7e362df24a7c49566fbbca4084dd4ee307
charlie <charlie@charlie-mac-mini.local>
2026-03-14 09:55:47 -0400

    Added oh-my-zsh setup to install.sh

--- install.sh
+++ install.sh
@@ -24,6 +24,33 @@ fi
 echo "Installing Homebrew packages..."
 brew bundle --file="$DOTFILES_DIR/Brewfile"
 
+# Install Oh My Zsh if not already installed
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Install Oh My Zsh custom plugins
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+OMZ_PLUGINS=(
+  "zsh-autosuggestions|https://github.com/zsh-users/zsh-autosuggestions"
+  "zsh-syntax-highlighting|https://github.com/zsh-users/zsh-syntax-highlighting"
+  "zsh-bat|https://github.com/fdellwing/zsh-bat"
+  "zsh-completions|https://github.com/zsh-users/zsh-completions"
+  "you-should-use|https://github.com/MichaelAquilina/zsh-you-should-use"
+)
+
+for entry in "${OMZ_PLUGINS[@]}"; do
+  plugin="${entry%%|*}"
+  url="${entry##*|}"
+  plugin_dir="$ZSH_CUSTOM/plugins/$plugin"
+  if [[ ! -d "$plugin_dir" ]]; then
+    echo "Installing Oh My Zsh plugin: $plugin..."
+    git clone "$url" "$plugin_dir"
+  fi
+done
+
 # Stow all config packages and setup symlinks
 cd "$DOTFILES_DIR"
 for dir in */; do
@@ -35,3 +62,4 @@ for dir in */; do
 done
 
 echo "Configuration complete!"
+
